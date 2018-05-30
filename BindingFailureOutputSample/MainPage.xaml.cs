8039b4756995fd42450e3e89d1b741a6662c8dd2
Gerald Versluis <gerald@verslu.is>
2018-05-30 12:56:18 +0200

    Initial Commit

---
+++ BindingFailureOutputSample/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace BindingFailureOutputSample
+{
+	public partial class MainPage : ContentPage
+	{
+		public string ExistingProperty
+		{
+			get;
+			set;
+		}
+
+		public MainPage()
+		{
+			InitializeComponent();
+
+			BindingContext = this;
+		}
+	}
+}
